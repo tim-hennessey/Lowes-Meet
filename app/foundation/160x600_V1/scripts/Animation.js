5aea10a75aeeea7e858cd612ba354c65defa98b7
tim-hennessey <tim.hennessey@deutschinc.com>
2020-04-15 10:43:23 -0700

    Organize File Structure

---
+++ app/foundation/160x600_V1/scripts/Animation.js
@@ -0,0 +1,46 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var tl = new TimelineMax();
+
+	var img = document.getElementById('img');
+	var headline = document.getElementById('headline');
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+		t.set(headline, {transformOrigin:"50% 20%"});
+
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+		tl.from(img, 5, {scale:1.1, ease: Sine.easeOut})
+		.from(headline, 5, {scale:.9, ease: Sine.easeOut},0);
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
